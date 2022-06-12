b30adb449339b1f2b566192444a6c169f373bd15
Mario Carera <marcar76@gmail.com>
2022-06-11 22:31:59 -0300

    v1.0.4

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import {FormularioComponent} from './components/formulario/formulario.component'
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: '/home', pathMatch: 'full'},
+  {path: 'contacto', component: FormularioComponent},
+  {path: 'home', component: AppComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
